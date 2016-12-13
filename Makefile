59d29384156a491e0c61d101733d8a1d18a87774
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-12-13 06:49:14 +0100

    Add Hyper’s configuration file

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ root := ${HOME}/.develop
 all:
 	@ln -Fis "${root}/gemrc" "${HOME}/.gemrc"
 	@ln -Fis "${root}/gitconfig" "${HOME}/.gitconfig"
+	@ln -Fis "${root}/hyper.js" "${HOME}/.hyper.js"
 	@ln -Fis "${root}/inputrc" "${HOME}/.inputrc"
 	@ln -Fis "${root}/tmux.conf" "${HOME}/.tmux.conf"
 	@grep -q '\.develop/bashrc' "${HOME}/.bash_profile" 2> /dev/null || \
---
+++ hyper.js
@@ -0,0 +1,41 @@
+module.exports = {
+  config: {
+    fontSize: 14,
+    fontFamily: 'Hack Regular, Monaco, Menlo, monospace',
+    cursorColor: 'rgba(248,28,229,0.8)',
+    cursorShape: 'BLOCK',
+    foregroundColor: '#fff',
+    backgroundColor: 'rgba(0,0,0,0.7)',
+    borderColor: '#333',
+    css: '',
+    termCSS: '',
+    showHamburgerMenu: false,
+    showWindowControls: '',
+    padding: '12px 14px',
+    colors: {
+      black: '#000000',
+      red: '#ff0000',
+      green: '#33ff00',
+      yellow: '#ffff00',
+      blue: '#0066ff',
+      magenta: '#cc00ff',
+      cyan: '#00ffff',
+      white: '#d0d0d0',
+      lightBlack: '#808080',
+      lightRed: '#ff0000',
+      lightGreen: '#33ff00',
+      lightYellow: '#ffff00',
+      lightBlue: '#0066ff',
+      lightMagenta: '#cc00ff',
+      lightCyan: '#00ffff',
+      lightWhite: '#ffffff'
+    },
+    shell: '',
+    shellArgs: ['--login'],
+    env: {},
+    bell: false,
+    copyOnSelect: false
+  },
+  plugins: ['hyperterm-tomorrow-night'],
+  localPlugins: []
+};
