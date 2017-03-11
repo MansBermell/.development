fc461e336a7298aac824583dff2ac63dd4a0863b
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-03-11 18:59:25 +0100

    Adjust Hyper’s selection color

--- hyper.js
+++ hyper.js
@@ -6,9 +6,17 @@ module.exports = {
   ],
   config: {
     bell: false,
-    fontSize: 14,
+    cursorBlink: true,
     fontFamily: 'Hack Regular, Monaco, Menlo, monospace',
-    termCSS: '.cursor-node { background: rgba(95, 216, 251, 0.7) !important; }',
+    fontSize: 14,
+    termCSS: `
+      ::selection {
+        background: rgb(95, 216, 251);
+      }
+      .cursor-node {
+        background: rgba(95, 216, 251, 0.7) !important;
+      }
+    `,
     workingDirectory: '~/Downloads',
   },
 };
