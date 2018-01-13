de7e385e20fc6d47584cd52cd72d6c56574fb3a9
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-01-13 10:09:59 +0100

    Simplify the ipynb filter

--- bin/ipynb_filter.py
+++ bin/ipynb_filter.py
@@ -4,73 +4,24 @@
 # https://github.com/toobaz/ipynb_output_filter
 
 import sys
+import json
 
-try:
-    runarg_idx = sys.argv.index('--rundir')
-    rundir = sys.argv[runarg_idx+1]
-    import os
-    os.chdir(os.path.expanduser(rundir))
-except ValueError:
-    pass
+from nbformat import reads, write
 
-version = None
+mapping = {
+    'execution_count': None,
+    'outputs': [],
+}
 
-if sys.version[0] == '2':
-    reload(sys)
-    sys.setdefaultencoding('utf8')
-
-try:
-    # Jupyter
-    from jupyter_nbformat import reads, write
-except ImportError:
-    try:
-        # New IPython
-        from nbformat import reads, write
-    except ImportError:
-        # Old IPython
-        from IPython.nbformat.current import reads, write
-        version = 'json'
-
-to_parse = sys.stdin.read()
-
-if not version:
-    import json
-    json_in = json.loads(to_parse)
-    version = json_in['nbformat']
-
-json_in = reads(to_parse, version)
-
-if hasattr(json_in, 'worksheets'):
-    # IPython
-    sheets = json_in.worksheets
-else:
-    # Jupyter
-    sheets = [json_in]
-
-for sheet in sheets:
+def clean(sheet):
     for cell in sheet.cells:
-        if "outputs" in cell:
-            cell.outputs = []
-        for field in ("prompt_number", "execution_number"):
+        for field in mapping:
             if field in cell:
-                del cell[field]
-        for field in ("execution_count",):
-            if field in cell:
-                cell[field] = None
+                cell[field] = mapping[field]
 
-        if "metadata" in cell:
-            for field in ("collapsed", "scrolled", "ExecuteTime"):
-                if field in cell.metadata:
-                    del cell.metadata[field]
+content = sys.stdin.read()
+version = json.loads(content)['nbformat']
+content = reads(content, version)
+clean(content)
 
-    if hasattr(sheet.metadata, "widgets"):
-        del sheet.metadata["widgets"]
-
-    if hasattr(sheet.metadata, "language_info"):
-        if hasattr(sheet.metadata.language_info, "version"):
-            del sheet.metadata.language_info["version"]
-
-if 'signature' in json_in.metadata:
-    json_in.metadata['signature'] = ""
-
-write(json_in, sys.stdout, version)
+write(content, sys.stdout, version)
